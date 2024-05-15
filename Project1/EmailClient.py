ee333f74003dd5520a68d6a054af80bd31ae346b
joewadk <jawadk7435@gmail.com>
2024-05-15 18:50:22 -0400

    email client done

--- Project1/EmailClient.py
+++ Project1/EmailClient.py
@@ -1,24 +1,20 @@
 from socket import *
-#zzoj ipci intn twat
-mailserver= "smtp.google.com" #server host we will be using
-serverPort=25 #port the socket uses (think of localhost:5000 when deploying a webapp)
-message='HELO \r\n' #handshake message
-dataCommand= 'DATA\r\n'
-
-emailFrom= input('Enter your email address: ')
-emailTo= input('Enter recipient email address: ')
-emailFromCommand='MAIL FROM: <%s>\r\n' %emailFrom
-emailToCommand='RCPT TO: <%s>\r\n' %emailTo
-
-clientSocket = socket(AF_INET, SOCK_STREAM) #note we use STREAM instead of DGRAM for the socket type 
-clientSocket.connect((mailserver,serverPort))
-clientSocket.send(message.encode())
-tcpResponse=clientSocket.recv(1024)
-print(tcpResponse)
-clientSocket.close()
-
-clientSocket.send(emailFromCommand.encode())
-clientSocket.send(emailToCommand.encode())
-clientSocket.send(dataCommand.encode())
-
-emailData= 'Subject: Comp Network Project 1 Test'
+from smtplib import SMTP_SSL
+mailserver= "smtp.gmail.com" #server host we will be using
+serverPort=465 #port the socket uses (think of localhost:5000 when deploying a webapp)
+username=input('Enter your email:\n') #note it only works on my email lol
+password='zzoj ipci intn twat' #this is the app password, you will have to go to your google account -> security ->2fa -> app passwords to set this up
+message_body=input('Enter message:\n')
+header= 'Subject: ' + input('Enter subject:\n')
+message=f"{header}\n\n{message_body}"
+recipient=input('Enter a recipient: \n')
+with SMTP_SSL(mailserver, serverPort) as server: #at this point, TCP has been established.
+    try:
+        server.ehlo() #identifier to esmtp server
+        #print(server.verify(mailserver))   
+        server.login(username,password) #login handler
+        server.sendmail(username,recipient,message)
+        server.quit() #close tcp
+        print("inshallah we send")
+    except:
+        print("womp womp")
